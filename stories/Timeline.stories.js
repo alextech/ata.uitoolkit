ea39251e4c554074ce39e8513384e1047fa4c22b
alextech <alex@alex-tech-adventures.com>
2021-06-29 15:43:46 +0300

    Возможность в storybook отображать ДОМ события.

--- stories/Timeline.stories.js
+++ stories/Timeline.stories.js
@@ -4,42 +4,58 @@ import event_1_Icon from './assets/dollar.png';
 export default {
   title: 'ATA/Timeline',
   argTypes: {
-
+    NewEventRequest: { action: 'NewEventRequest' }
   }
 }
 
-const EmptyTemplate = (args) =>
-`
-<ata-timeline 
-    years="${args.years}"
-    startingYear="${args.startingYear}"
-    age="${args.age}"
-/>`;
+const TimelineTemplate = (args) => {
+  const tpl = document.createElement('div');
+  let innerHtml =
+  `<ata-timeline
+      id="timelineCmp"
+      years="${args.years}"
+      startingYear="${args.startingYear}"
+      age="${args.age}"
+  >`;
 
-const TimelineWithEventTemplate = (args) => `
-<ata-timeline 
-    years="${args.years}" 
-    startingYear="${args.startingYear}"
-    age="${args.age}"
->
-    <ata-timeline-event event-id="sample_1" start="${args.eventStart}" end="${args.eventEnd}" icon="${args.icon}" slot="events" />
-</ata-timeline>`;
+  for (const event of args.events ?? []) {
+    innerHtml += `
+        <ata-timeline-event 
+            event-id="sample_1" 
+            start="${event.start}" end="${event.end}" icon="${event.icon}"
+            slot="events" />`;
+  }
 
-export const EmptyTimeline = EmptyTemplate.bind({});
+  innerHtml += '</ata-timeline>';
+  tpl.innerHTML = innerHtml;
+
+  const timelineEl = tpl.firstElementChild;
+  timelineEl.addEventListener('NewEventRequest', (e) => {
+    args.NewEventRequest(e.detail);
+  });
+
+  return timelineEl;
+}
+
+export const EmptyTimeline = TimelineTemplate.bind({});
 EmptyTimeline.args = {
   years: 15,
   age: 34,
-  startingYear: 2021
+  startingYear: 2021,
 }
 
-export const TimelineWithEvent = TimelineWithEventTemplate.bind({});
+export const TimelineWithEvent = TimelineTemplate.bind({});
 TimelineWithEvent.args = {
   years: 15,
   age: 34,
   startingYear: 2021,
 
-  eventStart: 2025,
-  eventEnd: 2033,
+  events: [
+    {
+      start: 2025,
+      end: 2033,
+      icon: event_1_Icon
+    }
+  ],
 
-  icon: event_1_Icon
 };
